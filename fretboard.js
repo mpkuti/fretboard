6cd785ee3bfa422e4aa20293cf2cf17e85f47f8d
Mika Kutila <mika.kutila@kovilta.fi>
2022-05-31 14:31:56 +0300

    modified:   fretboard.js modified:   index.html

--- fretboard.js
+++ fretboard.js
@@ -1,15 +1,15 @@
 const dataset = [10, 20, 30];
-const w = 500
-const h = 100
+const w = 500;
+const h = 100;
 
 const svg = d3.select("body").append("svg")
 	.attr("width", w)
 	.attr("height", h);
 
 svg.selectAll("circle")
-	.data("dataset")
+	.data(dataset)
 	.enter()
 	.append("circle")
-	.attr("cx", (d) => d)
-	.attr("cy", (d, i) => 10 * i)
-	.attr("r", (d) => d);
+	.attr("cx", (d) => 10 * d )
+	.attr("cy", (d, i) => h - 10 * i - 5 )
+	.attr("r", 5);
