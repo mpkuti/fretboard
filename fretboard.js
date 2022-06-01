85f7bb5f9a420c33e7cddf7916c65418879b08e5
Mika Kutila <mika.kutila@kovilta.fi>
2022-06-01 12:18:41 +0300

    add fret numbers + add fretboard dots

--- fretboard.js
+++ fretboard.js
@@ -1,7 +1,7 @@
 const dataset = [10, 20, 30];
 const w = 700;
 const h = 250;
-const padding = 10;
+const padding = 15;
 const no_frets = 13; // number of frets
 
 // COLOURS
@@ -11,10 +11,17 @@ const string_colour    = 'DarkGoldenRod';
 
 const notes = [ 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B' ];
 const intervals = [ "1", "p2", "S2", "p3", "S3", "4", '-5', "5", "p6", "S6", "p7", "S7" ];
+const dots = [3, 5, 7, 9, 15, 17, 19, 21];
+const double_dots = [12, 24];
 
+const fret_numbers = d3.range(1, no_frets + 1);
 const fret_h = h - 2 * padding; // height of one fret
 const fret_w = (w - 2 * padding) / no_frets; // width of one fret
 const string_sep = fret_h / 6;
+
+const xScale = d3.scaleLinear()
+.domain([1,no_frets])
+.range([padding + fret_w / 2, padding + (no_frets - 0.5) * fret_w]);
 
 function romanize(num) {
   var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
@@ -38,7 +45,38 @@ svg.append("rect")
 .attr("y", padding)
 .attr("width", w - 2 * padding)
 .attr("height", h - 2 * padding)
-.attr("fill", fretboard_colour);
+.attr("fill", fretboard_colour)
+.style("opacity", 1);
+
+// DOTS
+svg.append("g")
+.selectAll("circle")
+.data(dots)
+.enter()
+.append("circle")
+.attr("r", 10)
+.attr("cy", h / 2)
+.attr("cx", (d) => padding + 0.5 * fret_w + (d-1) * fret_w);
+
+// DOUBLE DOTS 1
+svg.append("g")
+.selectAll("circle")
+.data(double_dots)
+.enter()
+.append("circle")
+.attr("r", 10)
+.attr("cy", h / 3)
+.attr("cx", (d) => padding + 0.5 * fret_w + 11 * fret_w);
+
+// DOUBLE DOTS 2
+svg.append("g")
+.selectAll("circle")
+.data(double_dots)
+.enter()
+.append("circle")
+.attr("r", 10)
+.attr("cy", 2 * h / 3)
+.attr("cx", (d) => padding + 0.5 * fret_w + 11 * fret_w);
 
 // FRETS
 for ( var x=0; x<=no_frets; x++){
@@ -69,13 +107,14 @@ for (y=0; y<6; y++){
 }
 
 // FRET NUMBERS
-for ( var x=0; x<=no_frets; x++){
-	const fret_number = romanize(x+1);
-	svg.append("text")
-	.attr("text", romanize(x+1))
-	.attr("x", padding + 0.5 * fret_w + x * fret_w)
-	.attr("y", 0.5 * padding);
-}
+svg.selectAll("text")
+.data(fret_numbers)
+.enter()
+.append("text")
+.text((d) => romanize(d))
+.attr("x", (d) => xScale(d))
+.attr("y", h)
+.attr("text-anchor", "middle");
 
 svg.selectAll("circle")
 	.data(dataset)
