126a646ca48b1a9aad0032e11dc1a48ac490c537
Mika Kutila <mika.kutila@kovilta.fi>
2024-02-10 09:28:54 +0200

    new version with separate js files

---
+++ js/background.js
@@ -0,0 +1,134 @@
+// Import everything from common.js
+import * as common from './common.js';
+
+// Function to convert numbers to roman numerals
+// Source: https://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
+function romanize(num) {
+    var lookup = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+      },
+      roman = "",
+      i;
+    for (i in lookup) {
+      while (num >= lookup[i]) {
+        roman += i;
+        num -= lookup[i];
+      }
+    }
+    return roman;
+}
+
+// Function to draw the background
+export function drawBackground(svg) {
+    // Draw the background here
+    
+    // Create guitarneck rectangle
+    var guitarneck = svg.append("rect")
+        .attr("x", 0) // x position of the guitar neck
+        .attr("y", 0) // y position of the guitar neck
+        .attr("width", common.G_WIDTH) // width of the guitar neck
+        .attr("height", common.G_HEIGHT) // height of the guitar neck
+        .attr("fill", common.fretboard_color); // color of the guitar neck
+    
+    // Draw the frets according to the number of frets data
+    const fretNumbers = d3.range(0, common.NO_FRETS+1);
+    svg.selectAll("line")
+        .data(fretNumbers)
+        .enter()
+        .append("line")
+        .attr("x1", function(d) { return common.fretScale(d); }) // x position of the start of the line
+        .attr("y1", 0) // y position of the start of the line
+        .attr("x2", function(d) { return common.fretScale(d); }) // x position of the end of the line
+        .attr("y2", common.G_HEIGHT) // y position of the end of the line
+        .attr("stroke", common.fret_color) // color of the line
+        .attr("stroke-width", 3); // width of the line
+    
+    // Create a group for all the decotation dots 
+    var dots_group = svg.append("g")
+
+    // SINGLE DOTS
+    dots_group.selectAll(".single-dots")
+        .data(common.dots)
+        .enter()
+        .append("circle")
+        .attr("class", "single-dots")
+        .attr("r", common.dot_radius)
+        .attr("cy", common.G_HEIGHT/2)
+        .attr("cx", function(d) { return common.noteScale(d); });
+
+    // DOUBLE DOTS 1
+    dots_group.selectAll(".double-dots-1")
+        .data(common.double_dots)
+        .enter()
+        .append("circle")
+        .attr("class", "double-dots-1")
+        .attr("r", common.dot_radius)
+        .attr("cy", common.G_HEIGHT / 3)
+        .attr("cx", function(d) { return common.noteScale(d); });
+
+    // DOUBLE DOTS 2
+    dots_group.selectAll(".double-dots-2")
+        .data(common.double_dots)
+        .enter()
+        .append("circle")
+        .attr("class", "double-dots-2")
+        .attr("r", common.dot_radius)
+        .attr("cy", 2 * common.G_HEIGHT / 3)
+        .attr("cx", function(d) { return common.noteScale(d); });
+    
+    // Draw the strings
+    const stringNumbers = d3.range(0, 6);
+    // console.log(stringNumbers);
+    svg.selectAll(".string")
+        .data(stringNumbers)
+        .enter()
+        .append("line")
+        .attr("class", "string")
+        .attr("x1", 0) // x position of the start of the line
+        .attr("y1", (d) => common.G_HEIGHT/12 + common.G_HEIGHT * d/6) // y position of the start of the line
+        .attr("x2", common.G_WIDTH) // x position of the end of the line
+        .attr("y2", (d) => common.G_HEIGHT/12 + common.G_HEIGHT * d/6) // y position of the end of the line
+        .attr("stroke", common.string_color) // color of the line
+        .attr("stroke-width", (d) => common.stringThicknesses[d]); // width of the line
+    
+    // Fret numbers in roman numerals
+    svg.selectAll(".fret-numbers")
+        .data(fretNumbers)
+        .enter()
+        .append("text")
+        .attr("class", "fret-numbers")
+        .text((d) => romanize(d))
+        .attr("x", (d) => common.noteScale(d))
+        .attr("y", common.G_HEIGHT + 20)
+        .attr("text-anchor", "middle");
+    
+    // Add text labels for each note using all_note_coordinates
+    // x: all_note_coordinates[i][j].x
+    // y: all_note_coordinates[i][j].y
+    // note: all_note_coordinates[i][j].note
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < common.NO_FRETS + 2; j++) {
+            
+            svg.append("text")
+                .attr("x", common.all_note_coordinates[i][j].x)
+                .attr("y", common.all_note_coordinates[i][j].y)
+                .attr("dy", "0.35em") // vertical alignment
+                .text(common.all_note_coordinates[i][j].note)
+                .attr("text-anchor", "middle");
+        }
+    }
+
+
+}
